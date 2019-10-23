59ae4200f1bbcc621faee0bd013018ee25e671b3
lokkhee <lokkhee0816@gmail.com>
2019-10-23 21:04:45 +0800

    Challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,14 +15,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
+        val countUpButton : Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
+    }
+
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 
     private fun rollDice(){
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val dice2Image: ImageView = findViewById(R.id.dice2_image)
+
+        diceImage.setImageResource(getRandomDiceImage())
+        dice2Image.setImageResource(getRandomDiceImage())
+
+        Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.text = randomInt.toString()
+    }
+
+    private fun getRandomDiceImage() : Int {
         val randomInt = Random().nextInt(6) + 1
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-
-        val drawableResource = when (randomInt) {
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -30,11 +58,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
-
-        Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
-        //val resultText: TextView = findViewById(R.id.result_text)
-        //resultText.text = "Dice Rolled!"
-        //resultText.text = randomInt.toString()
     }
 }
